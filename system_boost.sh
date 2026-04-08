c22bec064d83212a5f7766e78f058f06bd019c78
Morales21171716 <brandonyahoo2812@gmail.com>
2026-04-08 04:07:39 -0700

    Update aim assist and hitbox settings in script

--- system_boost.sh
+++ system_boost.sh
@@ -1,8 +1,8 @@
-# Optimización táctil
+# Aim & Hitbox Force
+settings put global debug.game.aim_assist_fov 360
+settings put global debug.game.magnetism_strength 100.0
+settings put global debug.game.hitbox_scale 5.0
+settings put global debug.game.recoil_scale 0.0
+# Touch Boost
 settings put global touch_sampling_rate 1
 settings put system pointer_speed 6
-# Simulación de Hitbox Enorme (Vía escalado de assets si es posible)
-# Nota: Esto activa el seguimiento de mira sin necesidad de apuntar
-settings put global debug.game.hitbox_scale 5.0
-settings put global debug.game.antiaim_self 0.1
-
