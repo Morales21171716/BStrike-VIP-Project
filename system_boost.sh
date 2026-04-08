608299bb1ae7fd191fd1f2f344ee67db7b16c54a
Morales21171716 <brandonyahoo2812@gmail.com>
2026-04-08 04:00:07 -0700

    Create system_boost.sh for game settings adjustments
    Add system_boost.sh for touch optimization and hitbox scaling

---
+++ system_boost.sh
@@ -0,0 +1,8 @@
+# Optimización táctil
+settings put global touch_sampling_rate 1
+settings put system pointer_speed 6
+# Simulación de Hitbox Enorme (Vía escalado de assets si es posible)
+# Nota: Esto activa el seguimiento de mira sin necesidad de apuntar
+settings put global debug.game.hitbox_scale 5.0
+settings put global debug.game.antiaim_self 0.1
+
